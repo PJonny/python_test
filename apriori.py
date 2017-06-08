573f76f838dfc6d01e3775ecbbec42714168dc81
ponny <jjjj>
2017-06-08 22:50:01 +0800

    增添了‘memoizing.py'文件，为缓存修饰器详见《Python高级编程.pdf》P43。

--- apriori.py
+++ apriori.py
@@ -29,3 +29,27 @@ def scanD(D, ck, minSupport):
         supportData[key] = support
     return retLIst, supportData
 
+def aprioriGen(Lk, k):
+    retList = []
+    lenLk = len(LK)
+    for i in range(lenLK):
+        for j in range(i+1, lenLK):
+            L1 = list(Lk[i])[:k-2]; L2 = list(Lk[j])[:k-2]
+            L1.sort(); L2.sort()
+            if L1 == L2:
+                retList.append(Lk[i] | Lk[j])
+    return retList
+
+def apriori(dataSet, minSupport = 0.5):
+    C1 = createC1(dataSet)
+    D = map(set, dataSet)
+    L1, supportData = scanD(D, C1, minSupport)
+    L = [L1]
+    k = 2
+    while(len(L[k-2]) > 0):
+        CK = aprioriGen(L[k-2], k)
+        Lk, supK = scanD(D, Ck, minSupport)
+        supportData.update(supK)
+        L.append(Lk)
+        k += 1
+    return L, supportData
